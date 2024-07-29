8d43348d733e6a66b748f50a315c500c382c6007
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2024-07-29 06:07:47 -0300

    :rocket: Java RESTful API criada para a Sntander dev week

---
+++ santander-dev-week-2024/src/main/java/me/sousadiin/santander_dev_week_2024/Application.java
@@ -0,0 +1,14 @@
+package me.sousadiin.santander_dev_week_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("\n\nSunSet 4 Jesus!!");
+	}
+
+}
